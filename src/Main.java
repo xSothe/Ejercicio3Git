5f14b933446db6266a556991097501421834d117
Ángel Baz Monteiro <a22angelbm@ateis.local>
2022-11-09 20:58:44 +0100

    Introducir problema a solucionar

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,8 @@ public class Main {
 
         Empleado p = new Empleado("Pepe Pérez", new Date(90, Calendar.FEBRUARY, 1));
         System.out.println(p);
-    }
 
+            p.nombre = "Otro nombre";  // TODO: el nombre del empleado no debería poderse cambiar (debe ser inmutable)
+
+        }
 }
